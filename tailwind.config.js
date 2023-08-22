179bd7aafe28251c96abdabc17676331aeb72244
jatinder-singh221 <jatindersing19@gmail.com>
2023-08-22 15:21:00 +0530

    portfolio created

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero': "url('/ground.png')"
+      }
+    },
+  },
+  plugins: [],
+}
+
